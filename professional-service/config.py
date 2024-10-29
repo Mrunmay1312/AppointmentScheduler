63c8a04d956654cde3cd89e4078de1c62e3a532a
Mrunmay1312 <mnandanwar50@gmail.com>
2024-10-29 22:56:39 +0530

    added k8s deployment files

---
+++ professional-service/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:password@172.17.0.2/professionals'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ user-service/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:password@172.17.0.2/users'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
